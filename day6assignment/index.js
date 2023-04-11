ae115befd597c94aef47ce55514ee6ca2b0e0d5f
Amanchauhan99 <95541061+Amanchauhan99@users.noreply.github.com>
2023-04-11 14:47:58 +0530

    aman

---
+++ day6assignment/index.js
@@ -0,0 +1,13 @@
+var today=Date()
+console.log(today);
+
+console.log(today.getFullYear());
+console.log(today.getMonth());
+console.log(today.getMonth());
+
+var weekNumber = Math.ceil(today / 7);
+
+console.log(weekNumber);
+
+today.setFullYear(2020);
+console.log(today);
